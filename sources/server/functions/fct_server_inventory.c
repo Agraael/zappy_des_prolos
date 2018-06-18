795fba18770b7f31258e205cd590dcd0d8f38adf
Senior_Muchacho <lothaire.noah@epitech.eu>
2018-06-18 11:20:57 +0200

    push inventory and take with segfault

--- sources/server/functions/fct_server_inventory.c
+++ sources/server/functions/fct_server_inventory.c
@@ -11,13 +11,13 @@ int fct_server_inventory(char *cmd_line, int fd, t_env *e)
 {
 	char buff[4096];
 	
-	sprintf(buff,"[%s %i,%s %i,%s %i,%s %i,%s %i,%s %i]", "linemate",
+	sprintf(buff,"[%s %i,%s %i,%s %i,%s %i,%s %i,%s %i]\n", "linemate",
 		(int)e->inventory[fd].linemaute, "deraumere",
 		(int)e->inventory[fd].deraumere,"sibur",
 		(int)e->inventory[fd].sibur, "mendiane",
 		(int)e->inventory[fd].mendiane,"phiras",
 		(int)e->inventory[fd].phiras, "thystane",
-		(int)e->inventory[fd].thystane);
+		(int)e->inventory[fd].thystame);
 	dprintf(fd,"%s", buff);
 	(void)cmd_line;
 	return (0);
